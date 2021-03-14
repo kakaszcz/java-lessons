2a199aace287390ee1e96017d9956f2fe97b813b
Karola <karolinakaszczyszyn8@gmail.com>
2021-03-14 21:48:29 +0100

    reaging age added

--- lesson01/src/Main.java
+++ lesson01/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("karolina to bogini");
+        System.out.println("podaj swoj wiek: ");
+        int wiek = new Scanner(System.in).nextInt();
+        System.out.println("twoj wiek to: " + wiek);
     }
 }
