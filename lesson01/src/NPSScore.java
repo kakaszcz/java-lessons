5e28f210708185c238f9c58e18c0ea39112dbea5
Karola <karolinakaszczyszyn8@gmail.com>
2021-03-15 09:51:42 +0100

    oceny szczotek do kibla 01

--- lesson01/src/NPSScore.java
+++ lesson01/src/NPSScore.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class NPSScore {
+import java.util.Scanner;
+
+public class NPSScore {
+    public static void main(String[] args) {
+        System.out.println("twoja ocena naszych szczotek do kibla 1-10:");
+        int ocena = new Scanner(System.in).nextInt();
+        System.out.println("twoja ocena to:" + ocena);
+    }
 }
+
+
+
