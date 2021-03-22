6933c1fb79ba55e1dbc26a21a057cf4b9e61d772
Karola <karolinakaszczyszyn8@gmail.com>
2021-03-22 20:09:23 +0100

    change npsscore

--- lesson01/src/NPSScore.java
+++ lesson01/src/NPSScore.java
@@ -5,8 +5,13 @@ public class NPSScore {
         System.out.println("twoja ocena naszych szczotek do kibla 1-10:");
         int ocena = new Scanner(System.in).nextInt();
         System.out.println("twoja ocena to:" + ocena);
-        if ((ocena > 0) && (ocena < 6)){
+
+        if ((ocena > 0) && (ocena < 6)) {
             System.out.println("jesteś detractorem");
+        } else if ((ocena >= 7) && (ocena <= 8)) {
+            System.out.println("jestes neutralny");
+        } else if ((ocena >= 9) && (ocena <= 10)) {
+            System.out.println("promuszjesz nasze szczotki");
         }
     }
 }
